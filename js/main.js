55b2349b08a4cdb3ec3d2ccf90de184978bb0e73
Aitmuhamet Hojakeldiyev <92148785+Aitmuhamet@users.noreply.github.com>
2023-11-19 16:30:52 +0500

    header modified with adaptation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let navbarToggler = document.querySelector('.navbar-toggler')
 let siteHeaderNavLower = document.querySelector('.site-header__navbar_lower')
-let searchContainerButton = document.querySelector('.search-container__search')
-let searchContainerInput = document.querySelector('.search-container__input')
+let siteHeaderNavLstFirst = document.querySelector('.site-header__nav-list_first')
+let siteHeaderNavSecnd = document.querySelector('.site-header__nav-second')
+let searchContainerButton = document.querySelectorAll('.search-container__search')
 
 navbarToggler.addEventListener('click', function(){
     if (navbarToggler.classList.contains('open')){
@@ -13,14 +14,75 @@ navbarToggler.addEventListener('click', function(){
     }
 })
 
-searchContainerButton.addEventListener('click', function(){
-    if (searchContainerButton.classList.contains('open')){
-        searchContainerButton.classList.remove('open')
-        searchContainerInput.classList.add('search-container__input_disable')
-        searchContainerInput.style.transition = 'all 2s'
-    } else {
-        searchContainerButton.classList.add('open')
-        searchContainerInput.classList.remove('search-container__input_disable')
-        searchContainerInput.style.transition = 'all 2s'
+class SearchContainer {
+    constructor(el){
+        // super();
+        this.srchContainerMain = el.searchC
+
+        let buttonSearch = this.srchContainerMain.querySelector('.search-container__search')
+        let input = this.srchContainerMain.querySelector('.search-container__input')
+        let buttonCross = this.srchContainerMain.querySelector('.search-container__cross')
+        
+        buttonSearch.addEventListener('click', function(){
+            
+            const screenWidth = document.documentElement.clientWidth
+            if (screenWidth > 991) {
+                if (buttonSearch.classList.contains('open')){
+                    closeSearchInput(buttonSearch, input, buttonCross);
+                    siteHeaderNavSecnd.style.display = 'none'
+                    siteHeaderNavLstFirst.style.display = 'flex'
+                } else {
+                    openSearchInput(buttonSearch, input, buttonCross);
+                    siteHeaderNavSecnd.style.display = 'flex'
+                    siteHeaderNavLstFirst.style.display = 'none'
+                }
+            } else {
+                if (buttonSearch.classList.contains('open')){
+                    closeSearchInput(buttonSearch, input, buttonCross);
+                } else {
+                    openSearchInput(buttonSearch, input, buttonCross);
+                    console.log(buttonSearch);
+                }
+            }
+        })
     }
+
+    
+}
+
+const searchContainers = document.querySelectorAll('.search-container')
+searchContainers.forEach((item, key) => {
+    const numb = item.getAttribute('data-numb-container')
+    new SearchContainer({
+        searchC: item,
+        numbContr: numb
+    })
 })
+
+
+
+function openSearchInput(buttonS, input, buttonCr) {
+    buttonS.classList.add('open')
+    buttonCr.classList.remove('search-container__cross_disable')
+    input.classList.remove('search-container__input_disable')
+    input.style.transition = 'all 2s'
+}
+function closeSearchInput(buttonS, input, buttonCr) {
+    buttonS.classList.remove('open')
+    buttonCr.classList.add('search-container__cross_disable')
+    input.classList.add('search-container__input_disable')
+    input.style.transition = 'all 2s'
+}
+function onResize() {
+    const screenWidth = document.documentElement.clientWidth
+    let buttonSearchContainer = searchContainerButton[0]
+    
+    if (screenWidth < 991 || (screenWidth > 991 && buttonSearchContainer.classList.contains('open'))) {
+        siteHeaderNavSecnd.style.display = 'flex'
+        siteHeaderNavLstFirst.style.display = 'none'
+    } else {
+        siteHeaderNavSecnd.style.display = 'none'
+        siteHeaderNavLstFirst.style.display = 'flex'
+    }
+}
+window.addEventListener('resize', onResize)
